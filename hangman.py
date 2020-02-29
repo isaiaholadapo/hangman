25339877840631a5f628df66e6780761dea2b499
Isaiah Oladapao <isaiaholadapo18@gmail.com>
2020-02-29 20:37:51 +0100

    created list and hangman function

---
+++ hangman.py
@@ -0,0 +1,9 @@
+import random
+
+
+name = input("Type your name: ")
+print("Welcome ", name)
+print("---------------")
+print("Guess the right word in less than 10 entries ")
+word = random.choice(['Jane', 'Dru', 'Tope', 'Jumoke', 'Esther', 'Annie'])
+print(word)
